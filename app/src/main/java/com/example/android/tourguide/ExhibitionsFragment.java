cf9e57d199ded548da4c09b41c1f97c7316c8110
Anna Savina <anna.s.savina@gmail.com>
2018-06-18 23:51:49 +0300

    Fragments names correction. Working tabs and fragments

--- app/src/main/java/com/example/android/tourguide/ExhibitionsFragment.java
+++ app/src/main/java/com/example/android/tourguide/ExhibitionsFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ShopFragment extends Fragment {
+public class ExhibitionsFragment extends Fragment {
 
 
-    public ShopFragment() {
+    public ExhibitionsFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class ShopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        return inflater.inflate(R.layout.fragment_exhibitions, container, false);
     }
 
 }
--- app/src/main/java/com/example/android/tourguide/GuideAdapter.java
+++ app/src/main/java/com/example/android/tourguide/GuideAdapter.java
@@ -1,8 +1,54 @@
 package com.example.android.tourguide;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
 /**
  * Created by Sava on 18.06.2018.
  */
 
-public class GuideAdapter {
+public class GuideAdapter extends FragmentPagerAdapter {
+
+    /**
+     * Context of the app
+     */
+    private Context mContext;
+
+    public GuideAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MuseumsFragment();
+        } else if (position == 1) {
+            return new ExhibitionsFragment();
+        } else if (position == 2) {
+            return new ShopsFragment();
+        } else {
+            return new ParksFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_museums);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_exhibitions);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_shops);
+        } else {
+            return mContext.getString(R.string.category_parks);
+        }
+    }
 }
